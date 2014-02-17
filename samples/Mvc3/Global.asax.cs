b61244c4c8ca9838fded3f7fcc488953a0aef6d1
Max Toro <maxtoroq@gmail.com>
2014-02-17 01:45:32 -0300

    mvc3 samples

---
+++ samples/Mvc3/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MvcCodeRouting;
+
+namespace Samples {
+
+   public class MvcApplication : System.Web.HttpApplication {
+
+      void Application_Start() {
+
+         RegisterGlobalFilters(GlobalFilters.Filters);
+         RegisterRoutes(RouteTable.Routes);
+
+         ViewEngines.Engines.EnableCodeRouting();
+      }
+
+      void RegisterGlobalFilters(GlobalFilterCollection filters) {
+         filters.Add(new HandleErrorAttribute());
+      }
+
+      void RegisterRoutes(RouteCollection routes) {
+
+         routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+         routes.MapCodeRoutes(
+            rootController: typeof(Controllers.HomeController)
+         );
+
+         routes.MapCodeRoutes(
+            baseRoute: "Account",
+            rootController: typeof(MvcAccount.AccountController),
+            settings: new CodeRoutingSettings {
+               EnableEmbeddedViews = true,
+               Configuration = new MvcAccount.AccountConfiguration {
+                  AccountRepositoryResolver = () => new Models.TestAccountRepository(),
+                  PasswordServiceResolver = () => new MvcAccount.ClearTextPasswordService()
+               }
+            }
+         );
+      }
+   }
+}
