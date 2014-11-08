960cde2ed002a7f457a8d3dc17fbbe2378addd7f
Max Toro <maxtoroq@gmail.com>
2014-11-07 23:03:39 -0300

    moved sandcastle-md

--- build/docs/build-docs.bat
+++ build/docs/build-docs.bat
@@ -1,5 +1,7 @@
+cd %~dp0
+
 MSBuild.exe ..\..\src\MvcAccount\MvcAccount.csproj /p:Configuration=Release
 MSBuild.exe MvcAccount.shfbproj
-MSBuild.exe ..\..\submodules\sandcastle-md\sandcastle-md.sln
+MSBuild.exe sandcastle-md\sandcastle-md.sln
 rd /s /q ..\..\docs\api
-..\..\submodules\sandcastle-md\src\sandcastle-md\bin\Debug\sandcastle-md.exe output ..\..\docs\api
+sandcastle-md\src\sandcastle-md\bin\Debug\sandcastle-md.exe output ..\..\docs\api
